bad5b44b8d5069aba408a9eb37a21417c15fd17a
Nasim Dahman <nadahman@c2r6s4.42lausanne.ch>
2025-12-09 14:09:21 +0100

    test client/serv

---
+++ Serveur.hpp
@@ -0,0 +1,19 @@
+#ifndef SERVEUR_HPP
+#define SERVEUR_HPP
+
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+
+typedef uint32_t in_addr_t;
+typedef uint16_t in_port_t;
+uint32_t htonl(uint32_t hostlong);
+uint16_t htons(uint16_t hostshort);
+uint32_t ntohl(uint32_t netlong);
+uint16_t ntohs(uint16_t netshort);
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,18 +1,34 @@
-#include <iostream>
-#include <string>
+#include "Serveur.hpp"
 
 int main()
 {
+
+	int fd;
+
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	// addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_zero[(sizeof(addr))];
+
+	socklen_t client_len = sizeof(addr);
+
 	while (1)
 	{
-		std::string input;
-		std::cin >> input;
+		std::cout << "Attente de connexion.." << std::endl;
+		fd = socket(AF_INET, SOCK_STREAM, 0);
+		bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+		listen(fd, 1);
+		int acpt = accept(fd, (struct sockaddr*)&addr, &client_len);
 
-		if (input == "exit")
-			break;
-			
-		std::cout << "input : " << input << std::endl;
+		write(acpt, "Test\n", 5);
+
+		close(fd);
+		close(acpt);
 	}
+
 
 	return (0);
 }
