9b4d5db4f4dd2339e99ca1167bd4a621584e5f4c
cyraullie <cyril.goldenschue@gmail.com>
2025-12-10 15:53:14 +0100

    start read file

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,29 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 15:34:29 by cgoldens          #+#    #+#             */
-/*   Updated: 2025/12/10 15:34:29 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/12/10 15:41:53 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <list>
+#include <algorithm>
+
+int main(int ac, char **av)
+{
+	std::ifstream file;
+	std::string line;
+	std::string delimiter = " ";
+	std::string token;
+
+	std::cout << ac << " " << av[ac - 1] << std::endl;
+	file.open(av[ac - 1]);
+	while (std::getline(file, line))
+	{
+		std::cout << line << std::endl;
+	}
+}
