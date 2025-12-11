0be529306e34d3285ad8b85f0bc4207b5b5cc59c
Nasim Dahman <nas@MacBook-Air-de-Nasim.local>
2025-12-11 11:18:19 +0100

    update

--- Serveur.hpp
+++ Serveur.hpp
@@ -10,10 +10,10 @@
 
 typedef uint32_t in_addr_t;
 typedef uint16_t in_port_t;
-uint32_t htonl(uint32_t hostlong);
-uint16_t htons(uint16_t hostshort);
-uint32_t ntohl(uint32_t netlong);
-uint16_t ntohs(uint16_t netshort);
+// uint32_t htonl(uint32_t hostlong);
+// uint16_t htons(uint16_t hostshort);
+// uint32_t ntohl(uint32_t netlong);
+// uint16_t ntohs(uint16_t netshort);
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,12 +6,11 @@ int main()
 
 	int opt = 1;
 	setsockopt(socketServer, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
-
-	struct sockaddr_in address;
+	struct sockaddr_in address = {};
 	address.sin_family = AF_INET;
 	address.sin_port = htons(8080);
 	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_zero[(sizeof(address))];
+	// address.sin_zero[(sizeof(address))];
 
 	bind(socketServer, (struct sockaddr*)&address, sizeof(address));
 	std::cout << "Bind" << std::endl;
