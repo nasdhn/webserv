7fed0547639b15fed40bf3cf0d178db6f66f2de4
cyraullie <cyril.goldenschue@gmail.com>
2025-12-19 14:58:39 +0100

    add all print and setter

--- includes/Site.hpp
+++ includes/Site.hpp
@@ -4,23 +4,39 @@
 # include <string>
 # include <map>
 # include <vector>
+# include <iostream>
 
 class Site
 {
 	private:
 		std::string _name;
-		std::vector<std::string> _method;
+		std::vector<std::string> _methods;
 		bool _listDirectory;
 		std::string _defaultFile;
 		bool _uploadingFile;
 		std::string _root;
-		std::map<std::string, std::string> _redirection;
+		std::vector<std::string> _redirection;
 		std::string _cgi;
 	public:
 		Site();
 		~Site();
 
 		void setName(std::string n);
+		void setMethod(std::string s);
+		void setListDirectory(bool val);
+		void setDefaultFile(std::string s);
+		void setUploadingFile(bool val);
+		void setRoot(std::string s);
+		void setRedirection(std::string s);
+
+		void printMethods();
+		void printListDirectory();
+		void printDefaultFile();
+		void printUploadingFile();
+		void printRoot();
+		void printRedirection();
+		void printCGI();
+		void printData();
 
 };
 
--- srcs/parsing/Site.cpp
+++ srcs/parsing/Site.cpp
@@ -1,9 +1,106 @@
 #include "Site.hpp"
 
 
+Site::Site()
+{
+
+}
+
+Site::~Site()
+{
+
+}
 
 
 void Site::setName(std::string n)
 {
 	this->_name = n;
+}
+
+void Site::setMethod(std::string s)
+{
+	this->_methods.push_back(s);
+}
+
+void Site::setListDirectory(bool val)
+{
+	this->_listDirectory = val;
+}
+
+void Site::setDefaultFile(std::string s)
+{
+	this->_defaultFile = s;
+}
+
+void Site::setUploadingFile(bool val)
+{
+	this->_uploadingFile = val;
+}
+
+void Site::setRoot(std::string s)
+{
+	this->_root = s;
+}
+
+void Site::setRedirection(std::string s)
+{
+	this->_redirection.push_back(s);
+}
+
+void Site::printMethods()
+{
+	std::cout << "methods : " << std::endl;
+	for (std::vector<std::string>::iterator it = this->_methods.begin();
+		it != this->_methods.end();
+		++it)
+	{
+		std::cout << *it << std::endl;
+	}
+}
+
+void Site::printListDirectory()
+{
+	std::cout << "list directory :" << std::endl;
+	std::cout << this->_listDirectory << std::endl;
+}
+
+void Site::printDefaultFile()
+{
+	std::cout << "default file :" << std::endl;
+	std::cout << this->_defaultFile << std::endl;
+}
+
+void Site::printUploadingFile()
+{
+	std::cout << "uploading file :" << std::endl;
+	std::cout << this->_uploadingFile << std::endl;
+}
+
+void Site::printRoot()
+{
+	std::cout << "root :" << std::endl;
+	std::cout << this->_root << std::endl;
+}
+
+void Site::printRedirection()
+{
+	std::cout << "redirection : " << std::endl;
+	for (std::vector<std::string>::iterator it = this->_redirection.begin();
+		it != this->_redirection.end();
+		++it)
+	{
+		std::cout << *it << std::endl;
+	}
+}
+
+void Site::printCGI()
+{
+	std::cout << "CGI :" << std::endl;
+	std::cout << this->_cgi << std::endl;
+}
+
+void Site::printData()
+{
+	std::cout << "============= " << this->_name << " =============" << std::endl;
+	this->printMethods();
 }
