0e3fa9bbb7f0a71b1bebb36e684ce9886f749d54
Nasim Dahman <nas@MacBook-Air-de-Nasim.local>
2026-01-14 11:54:03 +0100

    add class request and method

---
+++ includes/Request.hpp
@@ -0,0 +1,59 @@
+#ifndef REQUEST_HPP
+#define REQUEST_HPP
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <cstdlib> // pour strtoul, etc.
+
+// On garde tes états, c'est très bien
+enum RequestState {
+    REQ_START_LINE,
+    REQ_HEADERS,
+    REQ_BODY,
+    REQ_COMPLETE,
+    REQ_ERROR
+};
+
+class Request {
+
+    public:
+        Request();
+        Request(const Request& other);
+        Request& operator=(const Request& other);
+        ~Request();
+
+        bool parse(const char* data, size_t size);
+
+        bool isComplete() const;
+        int  getErrorCode() const;
+
+        std::string getMethod() const;
+        std::string getPath() const;
+        std::string getVersion() const;
+        std::string getBody() const;
+        std::string getHeader(const std::string& key) const;
+
+        void setMaxBodySize(size_t size);
+
+    private:
+        std::string _method;
+        std::string _path;
+        std::string _httpVersion;
+        std::map<std::string, std::string> _headers;
+        std::string _body;
+
+        std::string _tempBuffer; 
+        size_t      _contentLength;
+        size_t      _maxBodySize;
+        RequestState _state;
+        int          _errorCode;
+
+        bool parseStartLine();
+        bool parseHeaders();
+        bool parseBody();
+        
+        void trim(std::string &s);
+};
+
+#endif
---
+++ srcs/InitServer/Request.cpp
@@ -0,0 +1,28 @@
+#include "Request.hpp"
+
+Request::Request()
+{
+
+}
+
+Request::Request(const Request& other)
+{
+
+}
+
+Request& Request::operator=(const Request& other)
+{
+
+}
+
+Request::~Request()
+{
+
+}
+
+
+
+bool Request::parse(const char* data, size_t size)
+{
+
+}
