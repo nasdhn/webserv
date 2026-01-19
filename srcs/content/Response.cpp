6a9f43eb2db1d269a64704ec99e74351f14d086c
Yann Oberson <yann@MacBook-Air-de-Yann.local>
2026-01-19 15:52:13 +0100

    oui

--- srcs/content/Response.cpp
+++ srcs/content/Response.cpp
@@ -1,45 +1,62 @@
 #include "../../includes/Response.hpp"
 #include <unistd.h>
+#include <fcntl.h>
 
-Response::Response() : _fd_body(-1), _status(200), _body("") {
+Response::Response() : _is_fd(false), _status(200), _fd_body(-1), _cgi_pid(-1), _body("") 
+{
     _headers["Server"] = "Webserv/1.0";
+    _headers["Connection"] = "keep-alive";
 }
 
-Response::~Response() {
+Response::~Response()
+{
     if (_is_fd && _fd_body != -1) {
         close(_fd_body);
     }
-    if (_cgi_pid == -1)
-    {
+    if (_cgi_pid != -1) {
         int status;
-        waitpid(_cgi_pid, &status, 0);
+        int result = waitpid(_cgi_pid, &status, WNOHANG);
+        if (result == 0) {
+            kill(_cgi_pid, SIGKILL);
+            waitpid(_cgi_pid, &status, 0);
+        }
     }
 }
 
-Response::Response(const Response &other) {
+Response::Response(const Response &other)
+{
     *this = other;
 }
 
-Response &Response::operator=(const Response &other) {
+Response &Response::operator=(const Response &other)
+{
     if (this != &other) {
+        _is_fd = other._is_fd;
         _status = other._status;
+        _fd_body = other._fd_body;
+        _cgi_pid = other._cgi_pid;
         _body = other._body;
         _headers = other._headers;
     }
     return *this;
 }
 
-void Response::setStatus(int code) {
+void Response::setStatus(int code)
+{
     _status = code;
 }
 
-void Response::setHeader(const std::string& key, const std::string& value) {
+void Response::setHeader(const std::string& key, const std::string& value)
+{
     _headers[key] = value;
 }
 
-void Response::setBody(const std::string& body) {
+void Response::setBody(const std::string& body)
+{
     _is_fd = false;
     _body = body;
+    _fd_body = -1;
+    
     std::stringstream ss;
     ss << body.length();
     setHeader("Content-Length", ss.str());
@@ -51,46 +68,62 @@ void Response::setBodyfd(int fd)
     _fd_body = fd;
 }
 
-std::string Response::_getStatusMessage(int code) const {
-    switch (code) {
-        case 200: return "OK";
-        case 201: return "Created";
-        case 400: return "Bad Request";
-        case 403: return "Forbidden";
-        case 404: return "Not Found";
-        case 500: return "Internal Server Error";
-        default: return "Unknown Status";
-    }
+void Response::setpid(pid_t pid)
+{
+    _cgi_pid = pid;
+}
+
+bool Response::is_fd() const
+{
+    return (_is_fd);
+}
+
+int Response::get_body_fd() const
+{
+    return _fd_body;
+}
+
+std::string Response::get_body_string() const
+{
+    return _body;
+}
+
+int Response::getStatus() const
+{
+    return _status;
 }
 
 std::string Response::get_header() const 
 {
     std::stringstream res;
     res << "HTTP/1.1 " << _status << " " << _getStatusMessage(_status) << "\r\n";
+    
     std::map<std::string, std::string>::const_iterator it;
     for (it = _headers.begin(); it != _headers.end(); ++it) {
         res << it->first << ": " << it->second << "\r\n";
     }
-    res << "\r\n";
     return res.str();
 }
 
-void Response::setpid(pid_t pid)
+std::string Response::getHeaderString() const
 {
-    _cgi_pid = pid;
+    return get_header();
 }
 
-bool Response::is_fd()
+std::string Response::_getStatusMessage(int code) const
 {
-    return (_is_fd);
-}
-
-int Response::get_body_fd()
-{
-    return _fd_body;
-}
-
-std::string Response::get_body_string()
-{
-    return _body;
+    switch (code) {
+        case 200: return "OK";
+        case 201: return "Created";
+        case 204: return "No Content";
+        case 301: return "Moved Permanently";
+        case 400: return "Bad Request";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 405: return "Method Not Allowed";
+        case 413: return "Payload Too Large";
+        case 500: return "Internal Server Error";
+        case 502: return "Bad Gateway";
+        default: return "Unknown Status";
+    }
 }
--- srcs/content/content.cpp
+++ srcs/content/content.cpp
@@ -6,188 +6,249 @@
 #include <sstream>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
-#include "../../includes/Response.hpp"
-#include "../../includes/Request.hpp"
-
 #include <fstream>
-#include <sstream>
-
-std::string getErrorPageContent(int code, Request req)
-{
-    if (req.error_pages.count(code) > 0) {
-        std::string path = req.error_pages[code];
-        std::ifstream file(path.c_str());
-        if (file.is_open()) {
-            std::stringstream buffer;
-            buffer << file.rdbuf();
-            return buffer.str();
-        }
-    }
-    std::stringstream ss;
-    ss << "<html><head><title>Error</title></head>";
-    ss << "<body><center><h1>" << code << "</h1></center>";
-    
-    if (code == 404) ss << "<center>Not Found</center>";
-    else if (code == 403) ss << "<center>Forbidden</center>";
-    else if (code == 405) ss << "<center>Method Not Allowed</center>";
-    else if (code == 500) ss << "<center>Internal Server Error</center>";
-    
-    ss << "</body></html>";
-    return ss.str();
-}
+#include <cstdlib>
+#include <cstring>
+#include "../../includes/Response.hpp"
+#include "Request.hpp"
+#include "Server.hpp"
+#include "Location.hpp"
 
 std::string getExtention(std::string path)
 {
-	std::string ext;
-	int empl = path.find_last_of(".");
-	if (empl == std::string::npos) 
+    size_t pos = path.find_last_of(".");
+    if (pos == std::string::npos) 
         return "";
-	ext = path.substr(empl);
-	return ext;
+    return path.substr(pos);
 }
 
-int execCGI(Request req, pid_t &pid)
+int execCGI(Request req, std::string fullPath, pid_t &pid)
 {
-	int fd_out[2];
-	int fd_in[2];
-	if (pipe(fd_out) == -1 || pipe(fd_in))
-	{
-		std::cerr << "Error: pipe" << std::endl;
-		return (-1);
-	}
-	pid = fork();
-	if (pid < 0)
-	{
-		std::cerr << "Error : fork" << std::endl;
-		return (-1);
-	}
-	if (pid == 0)
-	{
-		//process enfant
-		close(fd_out[0]);
-		dup2(fd_out[1], STDOUT_FILENO);
-		close(fd_out[1]);
-		close (fd_in[1]);
-		dup2(fd_out[0], STDOUT_FILENO);
-		close(fd_out[1]);
-		std::string ext = getExtention(req.getPath());
-		char *arg[3];
-		std::string bin;
-		if (ext == ".py")
-		{
-			bin = "/usr/bin/python3";
-		}
-		else if (ext == ".cgi" || ext == ".php")
-		{
-			bin = "/usr/bin/php-cgi";
-		}
-		else
-		{
-			exit (0);
-		}
-		arg[0] = (char *)bin.c_str();
-		arg[1] = (char *)req.getPath().c_str();
-		arg[2] = NULL;
-		std::vector<std::string> envs;
-    	envs.push_back("REQUEST_METHOD=" + req.getMethod());
-    	envs.push_back("QUERY_STRING=" + req.getQuery());
-    	envs.push_back("PATH_INFO=" + req.getPath());
-		char **envp = new char*[envs.size() + 1];
-    	for (size_t i = 0; i < envs.size(); i++) {
-        	envp[i] = (char*)envs[i].c_str();
-    	}
-    	envp[envs.size()] = NULL;
-		execve(bin.c_str(), arg, envp);
-		perror("execve FAILED");
-		delete[] envp;
-		exit (1);
-	}
-	else
-	{
-		//process parent
-		close(fd_out[1]);
-		close(fd_in[0]);
-		if (req.getMethod() == "POST" && !req.getBody().empty()) {
+    int fd_out[2];
+    int fd_in[2];
+
+    if (pipe(fd_out) == -1 || pipe(fd_in) == -1)
+    {
+        std::cerr << "Error: pipe" << std::endl;
+        return (-1);
+    }
+
+    pid = fork();
+    if (pid < 0)
+    {
+        std::cerr << "Error : fork" << std::endl;
+        return (-1);
+    }
+
+    if (pid == 0)
+    {
+        close(fd_out[0]);
+        dup2(fd_out[1], STDOUT_FILENO);
+        close(fd_out[1]);
+
+        close(fd_in[1]);
+        dup2(fd_in[0], STDIN_FILENO);
+        close(fd_in[0]);
+
+        std::string ext = getExtention(fullPath);
+        std::string bin;
+        
+        if (ext == ".py")
+            bin = "/usr/bin/python3";
+        else if (ext == ".cgi" || ext == ".php")
+            bin = "/usr/bin/php-cgi";
+        else
+            exit(1);
+
+        char *arg[3];
+        arg[0] = (char *)bin.c_str();
+        arg[1] = (char *)fullPath.c_str();
+        arg[2] = NULL;
+
+        std::vector<std::string> envs;
+        envs.push_back("REQUEST_METHOD=" + req.getMethod());
+        envs.push_back("QUERY_STRING=" + req.getQuery());
+        envs.push_back("PATH_INFO=" + req.getPath());
+        
+        if (!req.getBody().empty()) {
+             std::stringstream ss;
+             ss << req.getBody().size();
+             envs.push_back("CONTENT_LENGTH=" + ss.str());
+        }
+
+        char **envp = new char*[envs.size() + 1];
+        for (size_t i = 0; i < envs.size(); i++) {
+            envp[i] = (char*)envs[i].c_str();
+        }
+        envp[envs.size()] = NULL;
+
+        execve(bin.c_str(), arg, envp);
+        perror("execve FAILED");
+        delete[] envp;
+        exit(1);
+    }
+    else
+    {
+        close(fd_out[1]);
+        close(fd_in[0]);
+
+        if (req.getMethod() == "POST" && !req.getBody().empty()) {
             write(fd_in[1], req.getBody().c_str(), req.getBody().size());
         }
-		close(fd_in[1]);
-		return (fd_out[0]);
-	}
+        close(fd_in[1]);
+
+        return (fd_out[0]);
+    }
 }
 
-Response ft_delete(Response res, Request req)
+Response ft_delete(Response res, std::string fullPath, Server *server)
 {
-	struct stat info;
-	if (stat(req.getPath().c_str(), &info) != 0)
-	{
-		res.setStatus(404);
-		res.setBody("<h1>404 Not Found</h1>");
-		return res;
-	}
-	if (unlink(req.getPath().c_str()) == 0)
-	{
-		res.setStatus(204);
-		res.setBody("<h1>204 No Content</h1>");
-		return res;
-	}
-	return (res.setStatus(403), res);
+    struct stat info;
+    if (stat(fullPath.c_str(), &info) != 0)
+    {
+        res.setStatus(404);
+        res.setBody(getErrorPageContent(404, server));
+        return res;
+    }
+    if (unlink(fullPath.c_str()) == 0)
+    {
+        res.setStatus(204);
+        res.setBody(""); 
+        return res;
+    }
+    
+    res.setStatus(403);
+    res.setBody(getErrorPageContent(403, server));
+    return res;
 }
 
-Response BuildResponse(Request req)
+std::string getErrorPageContent(int code, Server *server)
 {
-	Response res;
-	std::string body;
-	struct stat info;
-	if (req.getMethod() == "DELETE")
-	{
-		return ft_delete(res, req);
-	}
-	if (stat(req.getPath().c_str(), &info) != 0)
-	{
-		res.setStatus(404);
-		res.setBody("<h1>404 Not Found</h1>");
-		return res;
-	}
-	if (S_ISDIR(info.st_mode))
-	{
-		res.setStatus(200);
-		res.setBody("<h1>It is a folder</h1>");
-		return res;
-	}
-	std::string ext = getExtention(req.getPath());
-	int content;
-	if (ext == ".py" || ext == ".php" || ext == ".cgi")
-	{
-		pid_t pid;
-		content = execCGI(req, pid);
-		if (content == -1)
-		{
-			res.setStatus(500);
-			return res;
-		}
-		res.setStatus(200);
-		res.setBodyfd(content);
-		res.setpid(pid);
-		return res;
-	}
-	if (req.getMethod() == "GET")
-	{
-		int fd = open(req.getPath().c_str(), O_RDONLY);
-		if (fd == -1)
-		{
-			res.setStatus(403);
-			return res;
-		}
-		res.setStatus(200);
-		res.setBodyfd(fd);
-		std::stringstream ss;
-    	ss << info.st_size;
-    	res.setHeader("Content-Length", ss.str());
-	}
-	else
-	{
-		res.setStatus(405);
-	}
-	return res;
+    if (server)
+    {
+        std::map<unsigned int, std::string> errorPages = server->getErrorPage();
+        if (errorPages.count(code) > 0)
+        {
+            std::string path = errorPages[code];
+            std::ifstream file(path.c_str());
+            if (file.is_open())
+            {
+                std::stringstream buffer;
+                buffer << file.rdbuf();
+                return buffer.str();
+            }
+        }
+    }
+    std::stringstream ss;
+    ss << "<html><head><title>Error</title></head><body><center><h1>" << code << "</h1></center></body></html>";
+    return ss.str();
+}
+
+Response BuildResponse(Request req, Server *server,Location *location)
+{
+    Response res;
+    struct stat info;
+    std::string fullPath;
+
+    if (server && req.getBody().size() > server->getMaxSize())
+    {
+        res.setStatus(413);
+        res.setBody(getErrorPageContent(413, server));
+        return res;
+    }
+
+    if (location)
+    {
+        std::vector<std::string> methods = location->getMethods();
+        bool allowed = false;
+        if (methods.empty()) allowed = true;
+        else {
+            for (size_t i = 0; i < methods.size(); i++) {
+                if (methods[i] == req.getMethod()) {
+                    allowed = true;
+                    break;
+                }
+            }
+        }
+        if (!allowed) {
+            res.setStatus(405);
+            res.setBody(getErrorPageContent(405, server));
+            return res;
+        }
+    }
+
+    if (location)
+        fullPath = location->getRoot() + req.getPath();
+    else if (server)
+        fullPath = server->getRoot() + req.getPath();
+    else
+        fullPath = "./www" + req.getPath();
+
+    if (req.getMethod() == "DELETE")
+    {
+        return ft_delete(res, fullPath, server);
+    }
+
+    if (stat(fullPath.c_str(), &info) != 0)
+    {
+        res.setStatus(404);
+        res.setBody(getErrorPageContent(404, server));
+        return res;
+    }
+
+    if (S_ISDIR(info.st_mode))
+    {
+        if (location && location->getAutoIndex())
+        {
+             res.setStatus(200);
+             res.setBody("<h1>Index of " + req.getPath() + "</h1>");
+             return res;
+        }
+        res.setStatus(403);
+        res.setBody(getErrorPageContent(403, server));
+        return res;
+    }
+
+    std::string ext = getExtention(fullPath);
+    if (ext == ".py" || ext == ".php" || ext == ".cgi")
+    {
+        pid_t pid;
+        int content = execCGI(req, fullPath, pid);
+        
+        if (content == -1)
+        {
+            res.setStatus(500);
+            res.setBody(getErrorPageContent(500, server));
+            return res;
+        }
+        res.setStatus(200);
+        res.setBodyfd(content);
+        res.setpid(pid);
+        return res;
+    }
+
+    if (req.getMethod() == "GET")
+    {
+        int fd = open(fullPath.c_str(), O_RDONLY);
+        if (fd == -1)
+        {
+            res.setStatus(403);
+            res.setBody(getErrorPageContent(403, server));
+            return res;
+        }
+        res.setStatus(200);
+        res.setBodyfd(fd);
+        
+        std::stringstream ss;
+        ss << info.st_size;
+        res.setHeader("Content-Length", ss.str());
+    }
+    else
+    {
+        res.setStatus(405);
+        res.setBody(getErrorPageContent(405, server));
+    }
+    return res;
 }
